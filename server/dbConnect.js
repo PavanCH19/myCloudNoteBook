17862e48228f826328489b1b67ece6e216501f24
Pavan C H <pavandvh27@gmail.com>
2025-02-06 18:08:24 +0530

    db connection added

---
+++ server/dbConnect.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const dbConnect = async () => {
+
+    if (!process.env.MONGO_URL) {
+        throw new Error('MONGO_URI is not defined in environment variables');
+    }
+
+    const conn = await mongoose.connect(process.env.MONGO_URL)
+        .then(() => console.log('MongoDB connected...'))
+        .catch((error) => {
+            console.error(`Error: ${error.message}`);
+            process.exit(1);
+        });
+
+    return conn;
+}
+
+module.exports = dbConnect;
+
+
+// const mongoose = require('mongoose');
+
+// if (!process.env.MONGO_URL) {
+//     throw new Error('MONGO_URI is not defined in environment variables');
+// }
+
+// const dbConnect = () => {
+//     mongoose.connect(process.env.MONGO_URL, () => {
+//         console.log('MongoDB connected...');
+//     });
+// }
+
+// module.exports = dbConnect;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
-const cors = require('cors');
+const dotenv = require('dotenv');
+const dbConnect = require('./dbConnect');
+
+dotenv.config();
 const app = express();
-require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
-app.use(cors());
 
+app.use(express.json());
+
+
+dbConnect();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! Welcome to the News API');
 });
 
-app.get('/pa', (req, res) => {
-    res.send('Hello');
-});
-console.log(port);
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
